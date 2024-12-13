838ed78e07bf6f3b21d4f12df927a34063dac087
David Reed <212698+dreed47@users.noreply.github.com>
2024-12-13 11:06:47 -0500

    update version info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
     name="redfin",  # How you named your package folder (MyLib)
-    version="0.1.1",  # Start with a small number and increase it with every change you make
+    version="0.1.2",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="A python wrapper around the unofficial redfin API.",  # Give a short description about your library
     long_description=long_description,
